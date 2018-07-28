38f4d93dadecba248f3968d020452759d5ca8d0f
Tobe O <thosakwe@gmail.com>
2018-07-28 00:39:56 -0400

    Options parser

--- src/frontend/Options.cpp
+++ src/frontend/Options.cpp
@@ -4,3 +4,36 @@
 //
 // Use of this source code is governed by an
 // MIT-style license that can be found in the LICENSE file.
+#include <sstream>
+#include "Options.h"
+
+bool jitdb::ParseOptions(jitdb::Options &options, int argc, const char **argv, const char **errorMessage) {
+    for (int i = 1; i < argc; i++) {
+        std::string arg(argv[i]);
+
+        if (i < argc - 2) {
+            if (arg == "--port" || arg == "-p") {
+                std::string next(argv[++i]);
+                std::istringstream iss(next);
+
+                if (iss >> options.port) {
+                    *errorMessage = "Invalid port number.";
+                    continue;
+                } else {
+                    return false;
+                }
+            } else if (arg == "--host" || arg == "-H") {
+                std::string next(argv[++i]);
+                options.host = next;
+                continue;
+            }
+        }
+
+        std::ostringstream oss;
+        oss << "Unknown argument '" << arg << "'.";
+        *errorMessage = oss.str().c_str();
+        return false;
+    }
+
+    return true;
+}
--- src/frontend/Options.h
+++ src/frontend/Options.h
@@ -7,4 +7,18 @@
 #ifndef JIT_DB_OPTIONS_H
 #define JIT_DB_OPTIONS_H
 
+#include <string>
+
+namespace jitdb
+{
+
+    struct Options
+    {
+        std::string host;
+        int port;
+    };
+
+    bool ParseOptions(jitdb::Options &options, int argc, const char **argv, const char **errorMessage);
+}
+
 #endif //JIT_DB_OPTIONS_H
